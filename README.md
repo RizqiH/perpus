de0cfbe1290f420a28d1a3ddf4c82ea43b88b989
joker joker george <rizkiamanan@gmail.com>
2022-04-29 22:55:23 +0700

    perpus2

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 #https://github.com/RizqiH/perpus.git 
+#https://github.com/RizqiH/perpuss.git
---
+++ praktikum_react2/src/pages/contact.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default class Contact extends React.Component{
+    render(){
+        return(
+            <div className='container'>
+                    <div className='alert alert-success'>
+                        <h1>Ini Halaman Contact</h1>
+                    </div>
+            </div>
+        
+
+        )
+    }
+
+}
